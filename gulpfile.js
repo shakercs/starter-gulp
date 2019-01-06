fa379e8831e154831b5d499a83a205af156cb856
Mahmoud Shaker <shakercs@gmail.com>
2019-01-06 15:34:40 +0300

    restructure

--- gulpfile.js
+++ gulpfile.js
@@ -10,10 +10,7 @@ const browsersync = require('browser-sync').create();
 const indexFolder = require('gulp-file-include');
 var fs = require('fs')
 
-
 function sass1() {
-
-  
     const variables = {
         // variables to sass
     };
@@ -22,31 +19,21 @@ function sass1() {
             'src/scss/bundle/style-ltr.scss'
 
         ])
-
         // .pipe(sassVars(variables, {
         //     verbose: true
         // }))
         .pipe(sass({
             //    outputStyle: 'compressed'
         }))
-      //  .pipe(autoprefixer('last 5 versions'))
-
+        //  .pipe(autoprefixer('last 5 versions'))
         .pipe(dest('dist/css'));
     //   .pipe(browsersync.stream());
-
 }
-
-
 function serve() {
     browsersync.init({
         server: 'dist'
     });
     watch(['src/**/*.scss'], sass1);
-  //  watch('src/**/*.html', indexFolder1);
-
+    //  watch('src/**/*.html', indexFolder1);
 }
-
-
-
-
 exports.default = series(sass1, serve);
